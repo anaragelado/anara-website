bf3a2d7a41415b4a1896ff6cb4b86b0fa74f6edb
Patrick Steger <patr.steger@gmail.com>
2026-04-27 12:20:52 +0100

    Instagram slider -> updated view of all 22 images

--- src/data/instagramPosts.ts
+++ src/data/instagramPosts.ts
@@ -18,6 +18,11 @@ export const instagramPosts: InstagramPost[] = [
     alt: "Bolo de Cenoura",
   },
   {
+    imagePath: "/assets/images/Instagram-Café c- Laranja Cardamomo e Brownie.webp",
+    url: PROFILE_URL,
+    alt: "Café com Laranja, Cardamomo e Brownie",
+  },
+  {
     imagePath: "/assets/images/Instagram-Cereja do Fundão.jpg",
     url: PROFILE_URL,
     alt: "Cereja do Fundão",
@@ -28,9 +33,34 @@ export const instagramPosts: InstagramPost[] = [
     alt: "Chocolate Masala com Laranja do Algarve",
   },
   {
+    imagePath: "/assets/images/Instagram-Chocolate ped. chocolate Framboesa.webp",
+    url: PROFILE_URL,
+    alt: "Chocolate com Pedaços de Chocolate e Framboesa",
+  },
+  {
     imagePath: "/assets/images/Instagram-Doce de Leite Argentino c -Amendoas.jpg",
     url: PROFILE_URL,
     alt: "Doce de Leite Argentino com Amêndoas",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Figo Pingo de Mel.webp",
+    url: PROFILE_URL,
+    alt: "Figo Pingo de Mel",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Kombucha de Maracijá.webp",
+    url: PROFILE_URL,
+    alt: "Kombucha de Maracujá",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Laranja do Alg. c- clementina e hortela.jpg",
+    url: PROFILE_URL,
+    alt: "Laranja do Algarve com Clementina e Hortelã",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Limao com Pepino e Hortela.jpg",
+    url: PROFILE_URL,
+    alt: "Limão com Pepino e Hortelã",
   },
   {
     imagePath: "/assets/images/Instagram-Manga com Coentros.webp",
@@ -38,13 +68,53 @@ export const instagramPosts: InstagramPost[] = [
     alt: "Manga com Coentros",
   },
   {
-    imagePath: "/assets/images/cone-strawberry-v2.webp",
+    imagePath: "/assets/images/Instagram-Mascarpone Manjericão.jpg",
     url: PROFILE_URL,
-    alt: "Morango",
+    alt: "Mascarpone com Manjericão",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Melancia Especial.webp",
+    url: PROFILE_URL,
+    alt: "Melancia Especial",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Morango Natas com calda Morango.webp",
+    url: PROFILE_URL,
+    alt: "Morango Natas com Calda de Morango",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Morango com pedaços Chocolate.webp",
+    url: PROFILE_URL,
+    alt: "Morango com Pedaços de Chocolate",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Natas com Toffee e Pinhoes.webp",
+    url: PROFILE_URL,
+    alt: "Natas com Toffee e Pinhões",
   },
   {
     imagePath: "/assets/images/Instagram-Pastel de Nata.jpg",
     url: PROFILE_URL,
     alt: "Pastel de Nata",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Requeijao c- Figos caramelizados.jpg",
+    url: PROFILE_URL,
+    alt: "Requeijão com Figos Caramelizados",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Salame de Chocolate.webp",
+    url: PROFILE_URL,
+    alt: "Salame de Chocolate",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Tarte de Maçã.webp",
+    url: PROFILE_URL,
+    alt: "Tarte de Maçã",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Tiramisu de Caramelo Sagado.webp",
+    url: PROFILE_URL,
+    alt: "Tiramisu de Caramelo Salgado",
   },
 ];
