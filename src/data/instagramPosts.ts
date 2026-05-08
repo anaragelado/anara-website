a885a961b1d78b9cd757b80b3e7adc3b2463d9fb
Patrick Steger <patr.steger@gmail.com>
2026-05-08 12:16:30 +0100

    Updated the instagram image slider order and added new hero video for desktop

--- src/data/instagramPosts.ts
+++ src/data/instagramPosts.ts
@@ -6,64 +6,14 @@ export interface InstagramPost {
 
 export const instagramPosts: InstagramPost[] = [
   {
-    imagePath: "/assets/images/Instagram-Baunilha com Abrunho.webp",
-    url: "https://www.instagram.com/p/C_SkTjps4S8",
-    alt: "Baunilha com calda de Abrunho",
-  },
-  {
     imagePath: "/assets/images/Instagram-Bolo de Cenoura.webp",
     url: "https://www.instagram.com/p/DH3J-05M0hR",
     alt: "Bolo de Cenoura",
   },
   {
-    imagePath: "/assets/images/Instagram-Café com Laranja Cardamomo e Brownie.webp",
-    url: "https://www.instagram.com/p/DFnHJF3sk7h",
-    alt: "Café com Laranja, Cardamomo e Brownie",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Cereja do Fundão.webp",
-    url: "https://www.instagram.com/p/C8l8NbdMkpW",
-    alt: "Cereja do Fundão",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Chocolate Masala com Laranja do Alg..webp",
-    url: "https://www.instagram.com/p/C7Wat_CsjGU",
-    alt: "Chocolate Masala com Laranja do Algarve",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Chocolate ped. chocolate Framboesa.webp",
-    url: "https://www.instagram.com/p/DAal88kM_hx",
-    alt: "Chocolate com Pedaços de Chocolate e Framboesa",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Doce de Leite com Amendoas 2.webp",
-    url: "https://www.instagram.com/p/C7wrNEusWB0",
-    alt: "Doce de Leite Argentino com Amêndoas",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Figo Pingo de Mel.webp",
-    url: "https://www.instagram.com/p/C-kG4Efssi1",
-    alt: "Figo Pingo de Mel",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Kombucha Maracujá 3.webp",
-    url: "https://www.instagram.com/p/DKMg5aWs2m5",
-    alt: "Kombucha de Maracujá",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Laranja e Clementina com Hortelã.webp",
-    url: "https://www.instagram.com/p/C58rU2nsKYp",
-    alt: "Laranja do Algarve com Clementina e Hortelã",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Limao com Pepino e Hortelã.webp",
-    url: "https://www.instagram.com/p/C9Ua8L4sZit",
-    alt: "Limão com Pepino e Hortelã",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Manga com Coentros.webp",
-    url: "https://www.instagram.com/p/C_kj1J7syOl",
-    alt: "Manga com Coentros",
+    imagePath: "/assets/images/Instagram-SalameChocolate2.webp",
+    url: "https://www.instagram.com/p/DSALI5sDSOd",
+    alt: "Salame de Chocolate",
   },
   {
     imagePath: "/assets/images/Instagram-Mascarpone com Manjericão.webp",
@@ -81,29 +31,14 @@ export const instagramPosts: InstagramPost[] = [
     alt: "Morango com Pedaços de Chocolate",
   },
   {
-    imagePath: "/assets/images/Instagram-MorangoNatas.webp",
-    url: "https://www.instagram.com/p/DKHX1qJMFRp",
-    alt: "Morango Natas com Calda de Morango",
+    imagePath: "/assets/images/Instagram-Kombucha Maracujá 3.webp",
+    url: "https://www.instagram.com/p/DKMg5aWs2m5",
+    alt: "Kombucha de Maracujá",
   },
   {
-    imagePath: "/assets/images/Instagram-Natas com Toffee e Pinhoes.webp",
-    url: "https://www.instagram.com/p/DGLKqDysnhx",
-    alt: "Natas com Toffee e Pinhões",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Pastel de Nata2.webp",
-    url: "https://www.instagram.com/p/CtRjzDht7d1",
-    alt: "Pastel de Nata",
-  },
-  {
-    imagePath: "/assets/images/Instagram-Requeijão com Figos.webp",
-    url: "https://www.instagram.com/p/CwiAgFvNur1",
-    alt: "Requeijão com Figos Caramelizados",
-  },
-  {
-    imagePath: "/assets/images/Instagram-SalameChocolate2.webp",
-    url: "https://www.instagram.com/p/DSALI5sDSOd",
-    alt: "Salame de Chocolate",
+    imagePath: "/assets/images/Instagram-Laranja e Clementina com Hortelã.webp",
+    url: "https://www.instagram.com/p/C58rU2nsKYp",
+    alt: "Laranja do Algarve com Clementina e Hortelã",
   },
   {
     imagePath: "/assets/images/Instagram-Tarte de Maçã.webp",
@@ -111,8 +46,73 @@ export const instagramPosts: InstagramPost[] = [
     alt: "Tarte de Maçã",
   },
   {
+    imagePath: "/assets/images/Instagram-Natas com Toffee e Pinhoes.webp",
+    url: "https://www.instagram.com/p/DGLKqDysnhx",
+    alt: "Natas com Toffee e Pinhões",
+  },
+  {
+    imagePath: "/assets/images/Instagram-MorangoNatas.webp",
+    url: "https://www.instagram.com/p/DKHX1qJMFRp",
+    alt: "Morango Natas com Calda de Morango",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Manga com Coentros.webp",
+    url: "https://www.instagram.com/p/C_kj1J7syOl",
+    alt: "Manga com Coentros",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Pastel de Nata2.webp",
+    url: "https://www.instagram.com/p/CtRjzDht7d1",
+    alt: "Pastel de Nata",
+  },
+  {
     imagePath: "/assets/images/Instagram-Tiramisù.webp",
     url: "https://www.instagram.com/p/DKZZZeMMA7t",
     alt: "Tiramisù de Caramelo Salgado",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Requeijão com Figos.webp",
+    url: "https://www.instagram.com/p/CwiAgFvNur1",
+    alt: "Requeijão com Figos Caramelizados",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Limao com Pepino e Hortelã.webp",
+    url: "https://www.instagram.com/p/C9Ua8L4sZit",
+    alt: "Limão com Pepino e Hortelã",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Baunilha com Abrunho.webp",
+    url: "https://www.instagram.com/p/C_SkTjps4S8",
+    alt: "Baunilha com calda de Abrunho",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Café com Laranja Cardamomo e Brownie.webp",
+    url: "https://www.instagram.com/p/DFnHJF3sk7h",
+    alt: "Café com Laranja, Cardamomo e Brownie",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Chocolate Masala com Laranja do Alg..webp",
+    url: "https://www.instagram.com/p/C7Wat_CsjGU",
+    alt: "Chocolate Masala com Laranja do Algarve",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Cereja do Fundão.webp",
+    url: "https://www.instagram.com/p/C8l8NbdMkpW",
+    alt: "Cereja do Fundão",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Doce de Leite com Amendoas 2.webp",
+    url: "https://www.instagram.com/p/C7wrNEusWB0",
+    alt: "Doce de Leite Argentino com Amêndoas",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Chocolate ped. chocolate Framboesa.webp",
+    url: "https://www.instagram.com/p/DAal88kM_hx",
+    alt: "Chocolate com Pedaços de Chocolate e Framboesa",
+  },
+  {
+    imagePath: "/assets/images/Instagram-Figo Pingo de Mel.webp",
+    url: "https://www.instagram.com/p/C-kG4Efssi1",
+    alt: "Figo Pingo de Mel",
   },
 ];
